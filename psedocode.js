49b274e8413567a23469b7fc0d921817977afe84
FionaN <fiona.nicdao@northwestern.edu>
2021-10-23 22:54:19 -0500

    worked on JS

---
+++ psedocode.js
@@ -0,0 +1,22 @@
+// btnA.innerHTML = questions[x].answer[0];
+        // btnB.innerHTML = questions[x].answer[1];
+        // btnC.innerHTML = questions[x].answer[2];
+        // btnD.innerHTML = questions[x].answer[3];
+
+    // document.querySelector('.button').removeChild(document.querySelector('.button').childNodes[0])
+    var button2 = document.createElement('button');
+
+
+// btn2.addEventListener("click", showQuestion2)
+
+// function showQuestion2() {
+//     console.log("bye");
+//     questionEl.innerHTML = questions[1].question;
+    // for (var i=0; i< questions[1].answer.length; i++) {
+    //     var button3 = document.createElement('button');
+    //     button3.textContent =questions[1].answer[i].text;
+    //     answersEl.appendChild(button3);
+    //     document.querySelector('.button').remove();
+    // }
+    // document.querySelector('.button').removeChild(document.querySelector('.button').childNodes[0])
+// }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // variables
 var timeEl = document.querySelector(".timer");
 var mainEl = document.querySelector(".main");
-var secondsLeft = 20;
+var secondsLeft = 100;
 var startquizButton = document.querySelector(".start");
 var activeIntro = document.querySelector('.activeIntro')
 var activeQuestion = document.querySelectorAll(".activeQuestion")
@@ -10,6 +10,12 @@ var hideRes = document.querySelector('.hideResults')
 var finalScoreEl = document.querySelector('.finalScore')
 var questionEl = document.querySelector('.questionText')
 var answersEl = document.querySelector('.answerList')
+var btnA = document.getElementById("#btnA")
+// var btnB = document.getElementById("#btnB")
+// var btnC = document.getElementById("#btnC")
+// var btnD = document.getElementById("#btnD")
+var feedback = document.querySelector(".feedback")
+
 // timer function
 startquizButton.addEventListener("click", setTime);
 function setTime() {
@@ -36,37 +42,53 @@ function startQuiz() {
     console.log('click');
     activeIntro.setAttribute('class','hide');
     hideQue.setAttribute('class','activeQuestion');
-    showQuestion1()
+    showQuestion()
 }
-// show each question and corresponding answers 
-function showQuestion1() {
+// show each question and answers 
+var x = 0
+function showQuestion() {
     console.log("hello");
-    questionEl.innerHTML = questions[0].question;
-    for (var i=0; i< questions[0].answer.length; i++) {
-        var button2 = document.createElement('button');
-        button2.textContent =questions[0].answer[i].text;
-        answersEl.appendChild(button2);
-        button2.classList.add('btn2')
-        document.querySelector('.button').remove();
-    }
-    // document.querySelector('.button').removeChild(document.querySelector('.button').childNodes[0])
+    var currentQuestion = questions[x];
+    questionEl.textContent = currentQuestion.question;
+
+    answersEl.innerHTML = "";
+    currentQuestion.answer.forEach(function(choice, i){
+        var btn3 = document.createElement('button');
+        btn3.setAttribute('class','btn3');
+        btn3.setAttribute('value', choice);
+        btn3.textContent = i +1 + "." + choice;
+        btn3.onclick = chooseAnswer;
+        answersEl.appendChild(btn3);
+    });
+
+
 }
-var button2 = document.createElement('button');
+// User choose right or wrong answer
+var btn2El = document.querySelectorAll('.btn2')
 
+function chooseAnswer() {
+    console.log("what up")
+    if (this.value !== questions[x].correctAnswer) {
+        secondsLeft -= 10;
+        if (secondsLeft <0) {
+            secondsLeft = 0;
+        }
+        timeEl.textContent = secondsLeft
+        feedback.textContent = "wrong!";
+    } else {
+        feedback.textContent = "right!";
+    }
+    feedback.setAttribute('class', 'feedback1');
 
-// btn2.addEventListener("click", showQuestion2)
-
-// function showQuestion2() {
-//     console.log("bye");
-//     questionEl.innerHTML = questions[1].question;
-    // for (var i=0; i< questions[1].answer.length; i++) {
-    //     var button3 = document.createElement('button');
-    //     button3.textContent =questions[1].answer[i].text;
-    //     answersEl.appendChild(button3);
-    //     document.querySelector('.button').remove();
-    // }
-    // document.querySelector('.button').removeChild(document.querySelector('.button').childNodes[0])
-// }
+    x++
+    if (x === questions.length) {
+        sendResults();
+        timeEl.setAttribute('class','hide');
+    }
+    else {
+        showQuestion()
+    }
+}
 
 
 // an array of questions with objects answers 
@@ -74,51 +96,51 @@ var questions = [
     {
         question:'What is the correct definition for JavaScript?',
         answer:[
-            {text:'A) code to structure a website and its content', wrong: false},
-            {text:'B) code to style website content', wrong: false},
-            {text:'C) progamming language that adds interactivity to your website', correct: true},
-            {text:'D) a type of coffee',wrong: false}
-            // C is correct
-        ]
+            'A) code to structure a website and its content',
+            'B) code to style website content',
+            'C) progamming language that adds interactivity to your website',
+            'D) a type of coffee'
+        ],
+        correctAnswer: 'C) progamming language that adds interactivity to your website'
     },
     {
-        question:'Which is NOT a data type',
-        answer:[
-            {text:'A) boolean',wrong: false},
-            {text:'B) string',wrong: false},
-            {text:'C) object',wrong: false},
-            {text:'D) abbreviation', correct: true}
-            // D is correct
-        ]
+        question: 'Which is NOT a data type',
+        answer: [
+            'A) boolean',
+            'B) string',
+            'C) object',
+            'D) abbreviation'
+        ],
+        correctAnswer:'D) abbreviation'    
     },
     {
-        question:'What is the correct defintion of an array?',
-        answer:[
-            {text:'A) a way to store groups of data in a single variable',correct: true},
-            {text:'B) a coding language',wrong: false},
-            {text:'C) have two values: true or false',wrong: false},
-            {text:'D) a series of characterss surrounded by quotes',wrong: false}
-            // A is correct
-        ]
+        question: 'What is the correct defintion of an array?',
+        answer: [
+            'A) a way to store groups of data in a single variable',
+            'B) a coding language',
+            'C) have two values: true or false',
+            'D) a series of characterss surrounded by quotes'
+        ],
+        correctAnswer: 'A) a way to store groups of data in a single variable'
     },
     {
-        question:'A useful tool used during development and debugging for printing content to the debugger is:',
-        answer:[
-            {text:'A) Javascript',wrong: false},
-            {text:'B) terminal/ bash',wrong: false},
-            {text:'C) for loops',wrong: false},
-            {text:'D) console.log',correct: true}
-            // D is correct
-        ]
+        question: 'A useful tool used during development and debugging for printing content to the debugger is',
+        answer: [
+            'A) Javascript',
+            'B) terminal/ bash',
+            'C) for loops',
+            'D) console.log'
+        ],
+        correctAnswer: 'D) console.log'
     },
     {
-        question:'The condition in an if/else statement is enclosed within ___.',
-        answer:[
-            {text:'A) quotes',wrong: false},
-            {text:'B) curly brackets',correct: true},
-            {text:'C) parenthesis',wrong: false},
-            {text:'D) square brackets',wrong: false}
-            // B is correct
-        ]
-    }
+        question: 'The condition in an if/else statement is enclosed within ___.',
+        answer: [
+            'A) quotes',
+            'B) curly brackets',
+            'C) parenthesis',
+            'D) sqaure brackets',
+        ],
+        correctAnswer: 'B) curly brackets'
+    },
 ]
